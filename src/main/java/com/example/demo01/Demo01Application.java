ae6827ad34a4652e1638788a1f8e9af05064ec50
王旭东 <786257865@qq.com>
2019-03-15 14:38:55 +0800

    首次提交

---
+++ src/main/java/com/example/demo01/Demo01Application.java
@@ -0,0 +1,20 @@
+package com.example.demo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.demo01.dao")
+public class Demo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo01Application.class, args);
+    }
+
+}
+
