43c099433d2e5543a18887bc77c93f29609cf41a
BF-201807151619\Administrator <wangxudong@iwubida.com>
2019-03-15 15:55:42 +0800

    fgf

--- src/main/java/com/example/demo01/utils/AndSpecification.java
+++ src/main/java/com/example/demo01/utils/AndSpecification.java
@@ -14,7 +14,6 @@ public class AndSpecification<T> extends AbstractSpecification<T> {
     public AndSpecification(List<Specification> specifications) {
         super(specifications);
     }
-    
 
     @Override
     protected Specification<T> combine(Specification<T> specs, Specification<T> next) {
